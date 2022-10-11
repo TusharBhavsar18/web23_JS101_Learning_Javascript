5a912258c8c4d0c68f2cdfb85b45e7cc96ec3d3d
TusharBhavsar18 <tusharbhavsar2011@gmail.com>
2022-10-11 07:25:42 +0530

    day-1-problem

---
+++ Day-1-Problem-Web-23-Introduction-to-JS-Variables-and-Data-Types/p2.js
@@ -0,0 +1,16 @@
+// Problem 2
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+let name = "Tushar Bhavsar"
+   console.log(name);
+
+let father_name = "Gopal Bhavsar"
+   console.log(father_name);
+
+let mother_name = "Dipali Bhavsar"
+   console.log(mother_name);
---
+++ Day-1-Problem-Web-23-Introduction-to-JS-Variables-and-Data-Types/p4.js
@@ -0,0 +1,55 @@
+// Problem 4
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+var style1 = "📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚";
+var style2 = "📚                                              📚";
+var style3 = "📚               ___ℝ𝔼ℙ𝕆ℝ𝕋 ℂ𝔸ℝ𝔻___              📚";
+var style4 = "📚                                              📚";
+var style5 = "📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚";
+var style6 = "📚                                              📚";
+var name = "📚 Name: Tushar Bhavsar                         📚";
+var school = "📚 School Name: Sarjai Damodar Kude Vidyalaya   📚";
+var grade = "📚 Grade: 84.40                                 📚";
+var section = "📚 Section: C                                   📚";
+var rollno = "📚 Roll No: 18                                  📚";
+var style7 = "📚                                              📚";
+var style8 = "📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚";
+var style9 = "📚             📚            📚                 📚";
+var submarktitale = "📚   Subject   📚   Mark's   📚   Total Mark's  📚";
+var style10 = "📚             📚            📚                 📚";
+var style11 = "📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚";
+var style12 = "📚             📚            📚                 📚";
+var submark1 = "📚   Science   📚     96     📚       100       📚";
+var submark2 = "📚     Math    📚     85     📚       100       📚";
+var submark3 = "📚   History   📚     93     📚       100       📚";
+var style13 = "📚             📚            📚                 📚";
+var style14 = "📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚📚";
+
+
+//---------------------------------------------//
+
+console.log(style1);
+console.log(style2);
+console.log(style3);
+console.log(style4);
+console.log(style5);
+console.log(style6);
+console.log(name);
+console.log(school);
+console.log(grade);
+console.log(section);
+console.log(rollno);
+console.log(style7);
+console.log(style8);
+console.log(style9);
+console.log(submarktitale);
+console.log(style10);
+console.log(style11);
+console.log(style12);
+console.log(submark1);
+console.log(submark2);
+console.log(submark3);
+console.log(style13);
+console.log(style14);
